2a86dd8413660fbe0228af644c85d3e9431115ed
Egemen Sener <egemensener98@gmail.com>
2024-09-02 21:16:30 +0300

    oto db migration code added!

--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03fda6860b16310e31fafc1dc2de7f7ae1ed166f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+327b6e91249d192de467e94aeb516aae13df75f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
+++ Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03fda6860b16310e31fafc1dc2de7f7ae1ed166f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+327b6e91249d192de467e94aeb516aae13df75f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
