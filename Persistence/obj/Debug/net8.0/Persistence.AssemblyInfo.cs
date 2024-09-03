e5a1c3b5334f6196981ce911f5994d5ebd36f4d6
Egemen Sener <egemensener98@gmail.com>
2024-09-03 19:58:29 +0300

    cors and api request

--- Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
+++ Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31a6162581d88788c55fb8b144298f65ac840150")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f2d0491d591447997de3c0df7e7c08b6f74a00d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
