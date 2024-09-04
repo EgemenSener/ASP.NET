b1056eaaae24719b160dd13590a42667f2a2142b
Egemen Sener <egemensener98@gmail.com>
2024-09-04 22:29:21 +0300

    create new activity

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,21 +1,30 @@
 using Application.Activities;
 using Domain;
-using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    public class ActivitiesController: BaseApiController
+    public class ActivitiesController : BaseApiController
     {
 
         [HttpGet]
-        public async Task<ActionResult<List<Activity>>> GetActivities() {
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
             return await Mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Activity>> GetActivityById(Guid id) {
-            return await Mediator.Send(new Details.Query{Id = id});
+        public async Task<ActionResult<Activity>> GetActivityById(Guid id)
+        {
+            return await Mediator.Send(new Details.Query { Id = id });
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateActivity(Activity activity)
+        {
+            await Mediator.Send(new Create.Command { Activity = activity });
+
+            return Ok();
         }
     }
 }
